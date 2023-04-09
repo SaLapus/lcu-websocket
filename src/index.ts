b31a38a564e10559480aaa6d3d63ab392d5a92f5
SaLapus <onaera@bk.ru>
2023-04-09 17:01:30 +0300

    Request logic add

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,134 @@
-import WebSocket from "ws";
+import { execSync } from "node:child_process";
 import EventEmitter from "node:events";
-import { AllEvents, AllEventMap } from "./types/Events";
-import LCU_Listener, { MESSAGE_TYPES, WAMPMessage } from "./types/Listener";
-import Cert from "./types/Cert";
+import https from "node:https";
 
-import { exec } from "node:child_process";
+import axios, { Axios, AxiosInstance } from "axios";
+import WebSocket from "ws";
+
+import Cert from "./types/Cert";
+import { EventMap, Events } from "./types/Events";
+import LCU_Listener, { MESSAGE_TYPES, WAMPMessage } from "./types/Listener";
+
+import { Paths, MethodsOf, InfoOf, PropertyOf } from "./types/Request/Info";
 
 interface Creds {
-  port: number;
+  username: "riot";
   password: string;
+  path: "127.0.0.1";
+  port: number;
 }
 
-export async function getWebSocketInterface() {
-  const creds = await new Promise((resolve: (arg0: Creds) => void, reject) => {
-    exec(
+export async function authenticate() {
+  try {
+    const options = execSync(
       `Get-CimInstance -Query "SELECT * from Win32_Process WHERE name LIKE 'LeagueClientUx.exe'" | Select-Object CommandLine | fl`,
-      { shell: "powershell.exe" },
-      (err, commandLine, stderr) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        const portRegex = /--app-port=([0-9]+)/;
-        const passwordRegex = /--remoting-auth-token=(.+?)(?= *"| --)/;
-
-        const [, port] = commandLine.match(portRegex)!;
-        const [, password] = commandLine.match(passwordRegex)!;
-        resolve({
-          port: Number(port),
-          password,
-        });
-      }
+      { shell: "powershell.exe", encoding: "utf-8" }
     );
-  });
 
-  return new RiotWSInterface(creds);
+    const portRegex = /--app-port=([0-9]+)/;
+    const passwordRegex = /--remoting-auth-token=(.+?)(?= *"| --)/;
+
+    const [, port] = options.match(portRegex)!;
+    const [, password] = options.match(passwordRegex)!;
+    return {
+      username: "riot",
+      password,
+      path: "127.0.0.1",
+      port: Number(port),
+    };
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 export class RiotWSInterface extends EventEmitter implements LCU_Listener {
   ws: WebSocket;
+  request: AxiosInstance;
 
-  constructor({ port, password }: Creds) {
+  constructor({ username, port, password }: Creds) {
     super();
 
-    const wsConnection = `wss://riot:${password}@127.0.0.1:${port}/`;
+    const wsConnection = `wss://${username}:${password}@127.0.0.1:${port}/`;
     this.ws = new WebSocket(wsConnection, "wamp", {
       ca: Cert,
     });
     this.ws.on("open", () => {});
     this.ws.on("message", handleWSMessage.bind(this));
+
+    this.request = axios.create({
+      baseURL: `https://127.0.0.1:${port}`,
+      auth: {
+        username,
+        password,
+      },
+      httpsAgent: new https.Agent({ ca: Cert }),
+      paramsSerializer: function (params) {
+        const seralizedParams: string[] = [];
+
+        for (const [key, param] of Object.entries(params)) {
+          if (param.length) {
+            seralizedParams.push(`${key}=[${param.join(",")}]`);
+          } else seralizedParams.push(`${key}=${param}`);
+        }
+
+        return seralizedParams.join("&");
+      },
+    });
   }
 
-  close() {
+  stop() {
     this.ws.close();
-  }
-
-  terminate() {
     this.ws.terminate();
   }
 
-  subscribe<LCUEvent extends AllEvents>(
-    event: LCUEvent,
-    cb: (data: AllEventMap[LCUEvent]["returnType"]) => void
-  ) {
+  async call<Path extends Paths, Method extends MethodsOf<Path>, Info extends InfoOf<Path, Method>>(
+    path: Path,
+    method: Method,
+    parameters?: PropertyOf<Info, "parameters"> & { [key: string]: any },
+    body?: PropertyOf<Info, "requestBody">
+  ): Promise<PropertyOf<Info, "responce"> | undefined> {
+    try {
+      let Path: string = path;
+      const Headers: {
+        JWT?: string;
+        "if-none-match"?: string;
+      } = {};
+      const Parameters: { [key: string]: any } = {};
+
+      if (parameters) {
+        const params = new Set(Object.keys(parameters));
+
+        for (const param of params) {
+          if (param === "JWT" || param === "if-none-match") Headers[param] = parameters[param];
+          else if (path.includes(`{${param}}`))
+            Path = path.replace(`{${param}}`, parameters[param]);
+          else {
+            Parameters[param] = parameters[param];
+          }
+        }
+      }
+
+      const res = await this.request({
+        url: Path,
+        method: method as string,
+        params: Parameters,
+        headers: Headers,
+        data: body,
+      });
+
+      return res.data;
+    } catch (e) {
+      console.error(e);
+      return undefined;
+    }
+  }
+
+  subscribe<LCUEvent extends Events>(event: LCUEvent, cb: (data: EventMap[LCUEvent]) => void) {
     super.addListener(event, cb);
     this.send(MESSAGE_TYPES.SUBSCRIBE, event);
   }
 
-  unsubscribe(topic: AllEvents, callback: (data: AllEventMap[AllEvents]["returnType"]) => void) {
+  unsubscribe(topic: Events, callback: (data: EventMap[Events]) => void) {
     super.removeListener(topic, callback);
     this.send(MESSAGE_TYPES.UNSUBSCRIBE, topic);
   }
@@ -85,7 +146,7 @@ function handleWSMessage(this: RiotWSInterface, message: WebSocket.RawData) {
   switch (data[0]) {
     case MESSAGE_TYPES.WELCOME:
       const [, session, idk, protocol] = data;
-      this.emit("ready");
+      this.emit("ready", this);
       break;
     case MESSAGE_TYPES.EVENT:
       const [, event, payload] = data;
@@ -97,3 +158,9 @@ function handleWSMessage(this: RiotWSInterface, message: WebSocket.RawData) {
       break;
   }
 }
+
+// const a = new RiotWSInterface();
+// a.call("/lol-summoner/v1/current-summoner/icon", "put", undefined, {
+//   inventoryToken: "rer",
+//   profileIconId: 1
+// })
